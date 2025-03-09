da8aa427640a7d40b09cfcd1d95ee163491075d3
Yuro Ortiz <ybo_24@hotmail.com>
2025-03-09 04:10:58 +0000

    Modificando middleware

--- myApp.js
+++ myApp.js
@@ -1,48 +1,10 @@
-//require('dotenv').config();
 let express = require('express');
 let app = express();
-
-/*app.get('/',(req,res) => {
-    res.send("Hello Express");
-});*/
-
-/*const absolutePath = __dirname + '/views/index.html';
-const public       = __dirname + '/public';*/
 
 app.use(function(req,res,next){
     console.log(req.method + ' ' + req.path + ' ' + req.ip)
     next();
 });
-
-
-/*app.use('/public',express.static(public));
-
-app.get('/',(req,res) => {
-    res.sendFile(absolutePath);
-});*/
-
-/*app.get('/json',(req,res) =>{
-    
-    if(process.env.MESSAGE_STYLE === "uppercase"){
-
-        res.send(
-            {
-                "message":"HELLO JSON"
-            }
-        )
-
-    }else{
-
-        res.send(
-            {
-                "message":"Hello json"
-            }
-        )
-
-    }
-    
-});*/
-
 
 
 
